24bc69fb377cdf5165c9552fb4a69b841a920319
Bartlomiej Kawecki <kawec11@gmail.com>
2022-06-13 10:53:29 +0200

    feat: miracle form implementation

---
+++ Miracle/Variant 1/main.js
@@ -0,0 +1,11 @@
+// const signUpContainer = document.querySelector("#sign-up");
+// const thankYouContainer = document.querySelector("#thank-you");
+// const form = document.querySelector(".newsletter__form");
+
+// const showThankYou = (e) => {
+//   e.preventDefault();
+//   signUpContainer.classList.add("display-none");
+//   thankYouContainer.classList.remove("display-none");
+// };
+
+// form.addEventListener("submit", showThankYou);
