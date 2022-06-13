9e8823830d6d680adac077896891cf681c8ddcee
Bartlomiej Kawecki <kawec11@gmail.com>
2022-06-13 14:29:55 +0200

    feat: open modal first time

--- Miracle/Variant 1/main.js
+++ Miracle/Variant 1/main.js
@@ -3,6 +3,7 @@ const thankYouContainer = document.querySelector("#thank-you");
 const form = document.querySelector(".form-modal .newsletter-form");
 const registerButton = document.querySelector("header .nav .last.leaf");
 const closeButton = document.querySelector(".form-modal .close-button");
+const isModalShown = sessionStorage.getItem("alreadyShow");
 
 const showThankYou = (e) => {
   e.preventDefault();
@@ -12,12 +13,22 @@ const showThankYou = (e) => {
 
 const showFormPopUp = () => {
   signUpPopUp.forEach((el) => el.classList.remove("display-none"));
+  sessionStorage.setItem("alreadyShow", "alredy shown");
 };
 
 const closeFormPopUp = () => {
   signUpPopUp.forEach((el) => el.classList.add("display-none"));
 };
 
+const showModalFirstTime = () => {
+  if (isModalShown !== "alredy shown") {
+    window.setTimeout(() => showFormPopUp(), 5000);
+  }
+};
+
 form.addEventListener("submit", showThankYou);
 registerButton.addEventListener("click", showFormPopUp);
 closeButton.addEventListener("click", closeFormPopUp);
+window.addEventListener("DOMContentLoaded", () => {
+  showModalFirstTime();
+});
