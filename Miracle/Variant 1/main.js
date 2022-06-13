af8c60131ca7e0cc7ba8650b2313cf66983b1ee3
Bartlomiej Kawecki <kawec11@gmail.com>
2022-06-13 13:14:17 +0200

    feat: js on click functions

--- Miracle/Variant 1/main.js
+++ Miracle/Variant 1/main.js
@@ -1,11 +1,23 @@
-// const signUpContainer = document.querySelector("#sign-up");
-// const thankYouContainer = document.querySelector("#thank-you");
-// const form = document.querySelector(".newsletter__form");
+const signUpPopUp = document.querySelectorAll(".sign-up");
+const thankYouContainer = document.querySelector("#thank-you");
+const form = document.querySelector(".newsletter-form");
+const registerButton = document.querySelector("header .nav .last.leaf");
+const closeButton = document.querySelector(".close-button");
 
-// const showThankYou = (e) => {
-//   e.preventDefault();
-//   signUpContainer.classList.add("display-none");
-//   thankYouContainer.classList.remove("display-none");
-// };
+const showThankYou = (e) => {
+  e.preventDefault();
+  signUpPopUp.classList.add("display-none");
+  thankYouContainer.classList.remove("display-none");
+};
 
-// form.addEventListener("submit", showThankYou);
+const showFormPopUp = () => {
+  signUpPopUp.forEach((el) => el.classList.remove("display-none"));
+};
+
+const closeFormPopUp = () => {
+  signUpPopUp.forEach((el) => el.classList.add("display-none"));
+};
+
+form.addEventListener("submit", showThankYou);
+registerButton.addEventListener("click", showFormPopUp);
+closeButton.addEventListener("click", closeFormPopUp);
