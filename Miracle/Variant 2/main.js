9dcea58f41c79c3f5b858e66c844d85812de9a2e
Bartlomiej Kawecki <kawec11@gmail.com>
2022-06-14 07:20:34 +0200

    feat: variant 2 ready

---
+++ Miracle/Variant 2/main.js
@@ -0,0 +1,40 @@
+const signUpPopUp = document.querySelectorAll(".sign-up");
+const thankYouContainer = document.querySelector("#thank-you");
+const form = document.querySelector(".form-modal .register-form");
+const registerButton = document.querySelector("header .nav .last.leaf");
+const closeButton = document.querySelector(".form-modal .close-button");
+const isModalShown = sessionStorage.getItem("alreadyShow");
+
+const showThankYou = (e) => {
+  e.preventDefault();
+  closeFormPopUp();
+  thankYouContainer.classList.remove("display-none");
+};
+
+const showFormPopUp = () => {
+  signUpPopUp.forEach((el) => el.classList.remove("display-none"));
+  sessionStorage.setItem("alreadyShow", "alredy shown");
+};
+
+const closeFormPopUp = () => {
+  signUpPopUp.forEach((el) => el.classList.add("display-none"));
+};
+
+const showModalFirstTime = () => {
+  if (isModalShown !== "alredy shown") {
+    window.setTimeout(() => showFormPopUp(), 5000);
+  }
+};
+
+registerButton.innerHTML = `<a
+                      class="join-enfamil jquery-once-20-processed"
+                      >Register account</a
+                    >
+                    `;
+
+form.addEventListener("submit", showThankYou);
+registerButton.addEventListener("click", showFormPopUp);
+closeButton.addEventListener("click", closeFormPopUp);
+window.addEventListener("DOMContentLoaded", () => {
+  showModalFirstTime();
+});
