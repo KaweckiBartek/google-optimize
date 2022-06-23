6fba82d4ad74a11b3720f78a1452f1e359e99cef
Bartlomiej Kawecki <kawec11@gmail.com>
2022-06-23 05:31:21 +0200

    feat: styling ready

--- Finish/Variant 2 - embedded/main.js
+++ Finish/Variant 2 - embedded/main.js
@@ -1,35 +1,9 @@
-const signUpToggleForm = document.querySelector(".newsletter--popup");
-const openButton = document.querySelector(".newsletter--popup-open-btn");
-const closeButton = document.querySelector(".newsletter--popup__close-button");
-const isModalShown = sessionStorage.getItem("alreadyShow");
-// const isLoggedIn = document.body.classList.contains("logged-in");
+const form = document.querySelector(".js-newsletter--embedded");
+const formProductsImage = document.querySelector(".newsletter__image-wrapper");
 
-const showFormPopUp = () => {
-  signUpToggleForm.classList.remove("hidden");
-  signUpToggleForm.classList.add("visible");
-  openButton.classList.remove("fade-in-down");
-  openButton.classList.add("fade-out-up");
-  sessionStorage.setItem("alreadyShow", "alredy shown");
+const showThankYou = (e) => {
+  e.preventDefault();
+  formProductsImage.classList.add("display-none");
 };
 
-const closeFormPopUp = () => {
-  signUpToggleForm.classList.add("hidden");
-  openButton.classList.remove("fade-out-up");
-  openButton.classList.add("fade-in-down");
-};
-
-const showModalFirstTime = () => {
-  if (isModalShown !== "alredy shown") {
-    window.setTimeout(() => showFormPopUp(), 5000);
-  }
-};
-
-if (document.readyState !== "loading") {
-  showModalFirstTime();
-} else {
-  document.addEventListener("DOMContentLoaded", function () {
-    showModalFirstTime();
-  });
-}
-openButton.addEventListener("click", showFormPopUp);
-closeButton.addEventListener("click", closeFormPopUp);
+form.addEventListener("submit", showThankYou);
